704fcba2e2fb8fee8de86f9d3b7c091a7c3900d4
Kaiyue Wang <kaiyuewang@berkeley.edu>
2017-10-24 10:22:01 -0700

    updates

--- js/grayscale.js
+++ js/grayscale.js
@@ -15,6 +15,12 @@
     }
   });
 
+  $('.nav-link').mouseover(function() {
+    $(this).addClass('animated').addClass('tada');
+    $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {$(this).removeClass('animated tada')});
+
+  })
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
